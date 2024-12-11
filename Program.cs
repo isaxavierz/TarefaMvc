7c134893f05cff0b7fc68a9588580dd123513ef2
isaxavierz <isabellyxavier1323@hotmail.com>
2024-12-11 22:12:23 +0000

    Atualização views

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession(options => 
+{
+    options.IdleTimeout =System.TimeSpan.FromSeconds(3600);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +25,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseSession();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
